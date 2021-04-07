c37026e18eece5bcd224b9751e83575c8c8ac163
Abigail Sexton <abigailsexton@Abigails-MBP.lan>
2021-04-06 17:02:47 -0700

    added in conditionals

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
 // Assignment code here
 
+// Function to capture the correct password length or continue on 
+var passLength = function() {
+	lengthValue = prompt("Choose Password Length Between: 8 - 128");
+	// prompt cannot be empty, be less than 8, more than 128 and cannot contain text
+	while (lengthValue === "" || parseInt(lengthValue) < 8 || parseInt(lengthValue) > 128 || isNaN(parseInt(lengthValue))) {
+		lengthValue = prompt("Choose Password Length Between: 8 - 128");
+	}
+	console.log("Password length: " + lengthValue);
+	return lengthValue;
+}
 
+function generatePassword() {
+  
+    passwordCharacters = "";
+    newPassword = "";
+      var passwordLength = pwLength(); // Length of Password Requestd
+      var lowerCase = window.confirm("Lowercase Letters?"); // Use Lowercase Letters?
+      var upperCase = window.confirm("Uppercase Letters?"); // Use Uppercase Letters?
+      var numbers = window.confirm("Numbers?"); // Use Numbers?
+    var special = window.confirm("Special Characters?"); // Use Special Characters?
+    
+    // If no character options are selected, alert message and begin again.
+    if (lowerCase === false && upperCase === false && numbers === false && special === false) {
+      window.alert("Please select at least one password criteria.");
+      generatePassword();
+}
 
+else { // Build password characters string per requirements.
+    if (lowerCase) {
+      passwordCharacters = "abcdefghijklmnopqrstuvwxyz";
+    }
+    if (upperCase) {
+      passwordCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (numbers) {
+      passwordCharacters += "0123456789";
+    }
+    if (special) {
+      passwordCharacters += " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    }
+       
+    console.log("Use Characters: " + passwordCharacters);
 
-
+    for (var i = 0; i < passwordLength; i++) {
+      newPassword += passwordCharacters.charAt(Math.floor(Math.random() * passwordCharacters.length));
+    }
+  }
+	console.log("Pasword is: " + newPassword);
+	return newPassword;
+}
 
 
 
